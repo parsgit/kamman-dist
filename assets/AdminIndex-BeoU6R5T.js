721092f8224db16b2c8b537fa8539d449525cdae
parsgit <nvioir@gmail.com>
2024-10-16 12:52:58 +0000

    update serverl url

---
+++ assets/AdminIndex-BeoU6R5T.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as c}from"./index-DJGDQ1D3.js";const o={},n={class:"h-full w-full flex justify-center items-center"},r=c("div",{class:"text-primary font-bold text-center text-4xl"},"Hello Admin",-1),a=[r];function l(_,d){return t(),s("div",n,a)}const f=e(o,[["render",l]]);export{f as default};
