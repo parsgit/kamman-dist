d6050666984b63c912c0f936b303ba5dedae82f2
parsgit <nvioir@gmail.com>
2024-10-16 13:50:15 +0000

    check test

--- assets/AdminIndex-DYc3BeLH.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as s,a as c}from"./index-DiW-Pznw.js";const o={},n={class:"h-full w-full flex justify-center items-center"},r=c("div",{class:"text-primary font-bold text-center text-4xl"},"Hello Admin",-1),a=[r];function l(_,d){return t(),s("div",n,a)}const f=e(o,[["render",l]]);export{f as default};
